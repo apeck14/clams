b2db99bf00fce5ff405915daedd0b131b3982413
apeck14 <aaronpeck_14@yahoo.com>
2021-03-10 22:09:33 -0600

    refactored code

--- commands/faq.js
+++ commands/faq.js
@@ -10,10 +10,24 @@ module.exports = {
 
         if(message.channel.id !== designatedChannelID) return message.channel.send(embed.setDescription(`Please use my commands in <#${designatedChannelID}>.`));
 
+        const questions = [
+            {q: "How often does the bot update matches?", a: "Every 5 mins."},
+            {q: "Which matches are stored?", a: "All matches are stored, but only race/final week matches count towards stats."},
+            {q: "How many matches are kept per player?", a: "Each players most recent 100 race/final week matches."},
+            {q: "How are ties handled on the leaderboard?", a: "If two players have the same W/L %, then the player with more total matches played will be placed above. If that is equal too, then whichever player currently has more trophies."},
+            {q: "What do underlined stats represent?", a: "The underlined stats count the matches within a duel individually. Not as 1 match, which would make an entire duel equal to a non-duel match."},
+            {q: "When did matches start being tracked?", a: "3/2/21"}
+        ];
+        let desc = "";
+
+        for(const q of questions){
+            desc += `**__Q: ${q.q}__**\nA: ${q.a}\n\n`;
+        }
+
         embed
             .setTitle("__FAQ__")
             .setFooter("Developed By: Apehk")
-            .setDescription("**Q: How often does the bot track matches?**\nA: Every 5 minutes.\n\n**Q: Which matches are stored?**\nA: Race day and final week matches.\n\n**Q: How many matches are stored per player?**\nA: Each player's last 100 matches.\n\n**Q: How are ties handled on the leaderboard?**\nA: The player who has more total matches played will be placed above. If that is equal too, then the player that has more trophies.\n\n**Q: What do underlined stats represent?**\nA: The stats with an underline calculate duel matches individually. (Not as 1 match, which would give an entire duel equal value to a non-duel match)\n\n**Q: When did matches start being tracked?**\nA: 3/2/21");
+            .setDescription(desc);
 
         message.channel.send(embed);
     }
