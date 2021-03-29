3f7879f7dcaf90904265b4eea3fe3bd28dcc255f
apeck14 <aaronpeck_14@yahoo.com>
2021-03-29 05:48:36 -0500

    bug fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoUtil = require('./util/mongoUtil');
 const { getMembers, tag, updateWarMatches, isColosseumWeek, isRaceDay, name, hex, logo } = require('./util/clanUtil');
 const { prefix } = require('./config.json');
 const { request, sortArrByDate, getMinsDiff } = require('./util/otherUtil');
-const { clanLogChannelID, missedAttacksChannelID, createAttacksEmbed, adminChannelID, commandsChannelID } = require('./util/serverUtil');
+const { clanLogChannelID, missedAttacksChannelID, createAttacksEmbed, adminChannelID, applyChannelID, commandsChannelID } = require('./util/serverUtil');
 const { setLastUpdated } = require('./util/lastUpdatedUtil');
 
 const bot = new Client();
@@ -146,9 +146,10 @@ bot.on('message', async message => {
     if(!bot.commands.has(command)) return;
 
     try{
-        if(bot.commands.get(command).adminCommand && message.channel.id !== adminChannelID) return message.channel.send(new MessageEmbed().setColor(hex).setDescription('You cannot use that command here!'));
-        else if(command !== 'apply' && message.channel.id !== commandsChannelID) return message.channel.send(new MessageEmbed().setColor(hex).setDescription('You cannot use that command here!'));
-        bot.commands.get(command).execute(message, args, bot);
+        if(bot.commands.get(command).adminCommand && message.channel.id === adminChannelID) bot.commands.get(command).execute(message, args, bot);
+        else if(command === 'apply' && message.channel.id === applyChannelID) bot.commands.get(command).execute(message, args, bot);
+        else if(command !== 'apply' && !bot.commands.get(command).adminCommand && message.channel.id === commandsChannelID) bot.commands.get(command).execute(message, args, bot);
+        else message.channel.send(new MessageEmbed().setColor(hex).setDescription('You cannot use that command here!'));
     } catch(err) {
         console.error(err);
     }
