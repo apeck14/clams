1129974e3c534f65a9696083b1b48ab3be1146d6
apeck14 <aaronpeck_14@yahoo.com>
2021-04-20 13:39:07 -0500

    bug fix

--- util/clanUtil.js
+++ util/clanUtil.js
@@ -404,7 +404,7 @@ const clanUtil = {
                 const date = parseDate(m.battleTime);
 
                 //non-colosseum (race match) on monday || colosseum matches
-                if((!m.isColosseumMatch && (date.getUTCDay() === 1 || (date.getUTCDay() === 2 && date.getUTCHours() < 10))) || m.isColosseumMatch){
+                if((!m.isColosseumMatch && (date.getUTCDay() === 1 || (date.getUTCDay() === 2 && date.getUTCHours() < 9))) || m.isColosseumMatch){
                     if(m.type === 'battle') (m.won) ? wins++ : losses++;
                     else if(m.type === 'duel'){
                         if(m.matchCount === 2) (m.won) ? wins += 2 : losses += 2;
