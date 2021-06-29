50da5ed485614d138f07ffef8fce5ce35f4b0f79
apeck14 <aaronpeck_14@yahoo.com>
2021-06-29 00:25:11 -0500

    bug fix

--- commands/stats.js
+++ commands/stats.js
@@ -52,7 +52,7 @@ module.exports = {
                 layout: {
                     padding: {
                         left: 0,
-                        right: 30,
+                        right: 50,
                         top: 0,
                         bottom: 0
                     }
